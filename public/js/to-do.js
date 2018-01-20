72200670b326c96d28b02fbdc1cdef32deddd928
LA The Fly Rebel <flyrebel@server.theflyrebel.com>
2018-01-20 11:57:27 -0500

    created user sign-in with validation

--- public/js/to-do.js
+++ public/js/to-do.js
@@ -10,6 +10,20 @@ $(document).ready(function() {
         	location.reload();
         	// console.log(data)
         });
+  });
+
+  $(document).on('click', '#submit-sign-in', function(event){
+  	event.preventDefault();
+  	let username = $('#username-sign-in').val().toLowerCase().trim();
+  	console.log(username);
+  	$.get('/api/users', function(data){
+  		data.forEach((results, index)=>{
+	  		console.log(results);
+	  		if(results.username === username){
+	            window.location.href = `/user/${results.id}/to-do`;
+	  		}
+  		})
+  	})
   })
 
 });
--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   // GET route for getting all of the posts
   app.get("/api/users/", function(req, res) {
-    db.Item.findAll()
+    db.User.findAll()
     .then(function(dbUser) {
       res.json(dbUser);
     });
